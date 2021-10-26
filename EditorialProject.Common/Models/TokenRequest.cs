7e1b3505a516543668eddc5f9237523f509bab08
jesus martinez carrera <martinez101jesus@outlook.com>
2021-10-26 11:17:18 -0500

    - Creación de Login

---
+++ EditorialProject.Common/Models/TokenRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace EditorialProject.Common.Models
+{
+    public class TokenRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ EditorialProject.Web/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿
+namespace EditorialProject.Web.Controllers
+{
+    using EditorialProject.Web.Helpers;
+    using EditorialProject.Web.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class AccountController : Controller
+    {
+        private readonly IUserHelper userHelper;
+
+        public AccountController(IUserHelper userHelper)
+        {
+            this.userHelper = userHelper;
+        }
+
+        public IActionResult Login()
+        {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                return this.RedirectToAction("Index", "Home");
+            }
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            var result = await this.userHelper.LoginAsync(loginViewModel.UserName, loginViewModel.Password, loginViewModel.RememberMe);
+
+            if (result.Succeeded)
+            {
+                if (this.Request.Query.Keys.Contains("ReturnUrl"))
+                {
+                    return this.Redirect(this.Request.Query["ReturnUrl"].First());
+                }
+                return this.RedirectToAction("Index", "Home");
+            }
+            this.ModelState.AddModelError(string.Empty, "Falló el Inicio de sesión");
+            return this.View(loginViewModel);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await this.userHelper.LogoutAsync();
+            return this.RedirectToAction("Index", "Home");
+        }
+    }
+}
